32dac0d89911bbac0a5a9c94bcae162b8bdcea52
ingeniumed <ingeniumed@users.noreply.github.com>
2024-11-02 21:23:16 +1100

    update the about me and home page

--- src/config.ts
+++ src/config.ts
@@ -3,12 +3,11 @@ import type { Site, SocialObjects } from "./types";
 export const SITE: Site = {
   website: "https://personal-website-beta-eight-51.vercel.app/", // replace this with your deployed domain
   author: "Gopal Krishnan",
-  profile: "https://gopalsmusings.wordpress.com",
   desc: "Just a bunch of thoughts, musings with a hint of coffee thrown in.",
-  title: "Personal Website",
+  title: "Gopal Krishnan",
   lightAndDarkMode: true,
   postPerIndex: 4,
-  postPerPage: 3,
+  postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: false,
 };
